66fe0a1c87427dfe7cf170d18662b5918cd4dfa4
Unknown <zhildemon@gmail.com>
2019-04-08 14:06:31 +1000

    check if not enough rows

--- simple.R
+++ simple.R
@@ -1,13 +1,19 @@
 handleblock <- function(df){
+    if(sum(df$WD) <= 1){
+        return(df)
+    }
     subdf <- df[,-c(1, 2, ncol(df)-1, ncol(df))]
     prox <- subdf
     prox$idx <- 1:nrow(df)
     prox$sums <- rowSums(subdf)
+    prox <- prox[order(-prox$sums),]
     
-    print(prox[order(-prox$sums),])
 }
 
-library(lubridate)
+if(!require(lubridate)){
+    install.packages("lubridate")
+    library(lubridate)
+}
 
 df <- read.csv("C:/Users/dv.zhilenkov/Desktop/SmtGoodCanWork/Yan/Shipment/Cars.csv", header = T, sep = ";")
 dw <- read.csv("C:/Users/dv.zhilenkov/Desktop/SmtGoodCanWork/Yan/Shipment/Wagons.csv", header = T, sep = ";")
@@ -19,7 +25,7 @@ df$year <- year(df$date)
 unique(df$year)
 
 proxdf <- df[df$numweek == 4, ]
-handleblock(proxdf)
+handleblock(as.data.frame(c(1,1)))
 
 #for(i in unique(df$year)){
   
